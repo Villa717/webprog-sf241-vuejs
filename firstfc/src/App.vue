fa65d40513f5914aefff335e1ae203b9bad92910
Andrei Antonio G Villa <agvilla@student.apc.edu.ph>
2026-02-01 13:07:06 +0800

    Task 03 Add view and insert comment/visitor feedback functionality to your website

--- firstfc/src/App.vue
+++ firstfc/src/App.vue
@@ -1,30 +1,12 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const instruments = ref([])
-
-async function getInstruments() {
-  const { data, error } = await supabase
-    .from('instruments')
-    .select()
-
-  if (error) {
-    console.error('Supabase error:', error)
-    return
-  }
-
-  instruments.value = data
-}
-
-onMounted(getInstruments)
-</script>
-
 <template>
-  <ul v-if="instruments.length">
-    <li v-for="i in instruments" :key="i.id">
-      {{ i.name }}
-    </li>
-  </ul>
-  <p v-else>No data loaded</p>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <personal-profile/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
+
+<script></script>
